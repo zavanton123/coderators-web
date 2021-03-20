660a017b7ccf36cc329cb5e07da28ab4cf6a95f3
zavanton123 <zavanton123@gmail.com>
2021-03-20 18:24:01 +0300

    [CDRW-0001]: register added

--- src/features/authentication/Register.tsx
+++ src/features/authentication/Register.tsx
@@ -1,13 +1,63 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {PageHeader} from "../common/PageHeader";
-import {Container} from "reactstrap";
+import {Button, Container} from "reactstrap";
 
 export const Register = () => {
+  const [username, setUsername] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('')
+
+  const onUsernameChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setUsername(event.target.value);
+  };
+
+  const onEmailChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setEmail(event.target.value);
+  };
+
+  const onPasswordChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setPassword(event.target.value);
+  };
+
+  const onSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    console.log(`zavanton - onSubmit: ${username} -> ${email} -> ${password}`);
+  };
+
   return (
     <>
       <PageHeader title="Register"/>
       <Container>
-        <h1>Register</h1>
+        <form onSubmit={onSubmit}>
+          <label>Username</label>
+          <br/>
+          <input
+            className="form-control"
+            value={username}
+            onChange={onUsernameChange}
+            type="text"/>
+          <br/>
+          <label>Email</label>
+          <br/>
+          <input
+            className="form-control"
+            value={email}
+            onChange={onEmailChange}
+            type="email"/>
+          <br/>
+          <label>Password</label>
+          <br/>
+          <input
+            className="form-control"
+            value={password}
+            onChange={onPasswordChange}
+            type="password"/>
+          <br/>
+          <div>
+            <Button color="primary">Register</Button>
+            <br/>
+          </div>
+        </form>
       </Container>
     </>
   );
