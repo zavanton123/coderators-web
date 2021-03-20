13f893c878c846dea871b4b3503b4f6eb9838f85
zavanton123 <zavanton123@gmail.com>
2021-03-20 18:20:33 +0300

    [CDRW-0001]: login form added

--- src/features/authentication/Login.tsx
+++ src/features/authentication/Login.tsx
@@ -1,14 +1,50 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {PageHeader} from "../common/PageHeader";
-import {Container} from "reactstrap";
+import {Button, Container} from "reactstrap";
 
 export const Login = () => {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('')
+
+  const onUsernameChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setUsername(event.target.value);
+  };
+
+  const onPasswordChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setPassword(event.target.value);
+  };
+
+  const onSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    console.log(`zavanton - onSubmit: ${username} -> ${password}`);
+  };
 
   return (
     <>
       <PageHeader title="Login"/>
       <Container>
-        <h1>Login</h1>
+        <form onSubmit={onSubmit}>
+          <label>Username</label>
+          <br/>
+          <input
+            className="form-control"
+            value={username}
+            onChange={onUsernameChange}
+            type="text"/>
+          <br/>
+          <label>Password</label>
+          <br/>
+          <input
+            className="form-control"
+            value={password}
+            onChange={onPasswordChange}
+            type="password"/>
+          <br/>
+          <div>
+            <Button color="primary">Login</Button>
+            <br/>
+          </div>
+        </form>
       </Container>
     </>
   );
