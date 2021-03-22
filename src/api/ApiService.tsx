aadb9a4e7a10cc650dccd5f115c0d1f9c6b2086e
zavanton123 <zavanton123@gmail.com>
2021-03-22 09:55:40 +0300

    [CDRW-0001]: jwt header added

--- src/api/ApiService.tsx
+++ src/api/ApiService.tsx
@@ -1,7 +1,10 @@
-import axios from "axios";
+import axios, {AxiosResponse} from "axios";
 import {LoginRequestParams} from "../features/authentication/AuthModels";
+import {ACCESS_TOKEN, JWT} from "../features/common/Constants";
 
 const BASE_URL = "http://127.0.0.1:9999/api";
+
+const responseToData = (response: AxiosResponse) => response.data
 
 export const authService = {
   userLogin: (loginParams: LoginRequestParams) => {
@@ -13,35 +16,39 @@ export const authService = {
   }
 };
 
-// todo zavanton - add jwt header!!!
 export const profileService = {
   fetchInfo: () => {
-    return axios.get(`${BASE_URL}/users/me`)
-      .then(response => response.data)
+    const accessToken = localStorage.getItem(ACCESS_TOKEN);
+    return axios.get(`${BASE_URL}/users/me`, {
+      headers: {
+        Authorization: `${JWT} ${accessToken}`
+      }
+    })
+      .then(responseToData);
   }
 }
 
 export const snippetService = {
   getAllSnippets: () => {
     return axios.get(`${BASE_URL}/snippets/`)
-      .then(response => response.data);
+      .then(responseToData);
   },
   getSnippetById: (id: number) => {
     return axios.get(`${BASE_URL}/snippets/${id}`)
-      .then(response => response.data);
+      .then(responseToData);
   }
 };
 
 export const categoryService = {
   getAllCategories: () => {
     return axios.get(`${BASE_URL}/categories/`)
-      .then(response => response.data);
+      .then(responseToData);
   }
 };
 
 export const tagService = {
   getAllTags: () => {
     return axios.get(`${BASE_URL}/tags/`)
-      .then(response => response.data);
+      .then(responseToData);
   }
 };
--- src/features/common/Constants.tsx
+++ src/features/common/Constants.tsx
@@ -1,2 +1,3 @@
 export const ACCESS_TOKEN = 'ACCESS_TOKEN';
 export const REFRESH_TOKEN = 'REFRESH_TOKEN';
+export const JWT = "JWT";
